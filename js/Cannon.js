5f7a34e2b0dd59e4a5ef22ae3a2952e84d18d75a
xPh0t0 <52384505+xPh0t0@users.noreply.github.com>
2021-09-12 22:15:16 -0400

    Add files via upload

---
+++ js/Cannon.js
@@ -0,0 +1,11 @@
+class Cannon {
+  constructor(x, y, width, height, angle) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.angle = angle;
+    
+  }
+  
+}
